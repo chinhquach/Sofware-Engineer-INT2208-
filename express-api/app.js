a6568637ec5934396c776037e9aeeef745ea4a52
chinhquach <112382255+chinhquach@users.noreply.github.com>
2024-04-29 15:49:58 +0000

    second-commit

---
+++ express-api/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+app.get('/', (request, response) => {
+    console.log(`URL: ${request.url}`);
+    response.send('Hello, Server!');
+});
+// Start the server 
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+    console.log(`Server listening on port ${server.address().port}`);
+});
+app.use(express.static(__dirname + '/'));
