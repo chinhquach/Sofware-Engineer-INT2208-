72002c07a8af33a0dd7c49cb5a2afe9ca8535e18
chinhquach <112382255+chinhquach@users.noreply.github.com>
2024-05-04 15:49:41 +0000

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+
+import Dicionary from "./components/Dictionary";
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <h1 className="heading">Dictionary</h1>
+        </header>
+        <main>
+          <Dicionary defaultKeyword="aesthetic"/>
+        </main>
+        <footer className="mt-5 footer">
+        
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
